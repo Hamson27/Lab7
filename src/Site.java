a1ce232e59e744a008da5be0ae767f0eb924a4ec
hamson <hamsonolotu@cmail.carleton.ca>
2025-11-07 15:53:19 -0500

    Initial implementation

--- src/Site.java
+++ src/Site.java
@@ -1,3 +1,11 @@
-public class Site {
+public abstract class Site {
     protected static final double TAX_RATE = 0.13;
+
+    public abstract double getBaseAmount();
+
+    public abstract double getTaxAmount();
+
+    public double getBillableAmount(){
+        return getBaseAmount() + getTaxAmount();
+    }
 }
